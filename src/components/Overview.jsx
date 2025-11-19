cd6d004f7492e2e29ace31bf4c6b07a03fffdf46
Rahul Raj <rr7426151@gmail.com>
2025-11-19 13:40:23 +0530

    upadate-tailwind

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -83,7 +83,7 @@ const Overview = () => {
                 </div>
                 <div className="ml-5 w-0 flex-1">
                   <dl>
-                    <dt className="text-sm font-medium text-gray-500 truncate">{stat.name}</dt>
+                    <dt className="text-sm font-medium text-gray-500 truncate ">{stat.name}</dt>
                     <dd className="flex items-baseline">
                       <div className="text-2xl font-semibold text-gray-900">{stat.value}</div>
                       {stat.change !== '0' && (
